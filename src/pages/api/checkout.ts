e0108d5bded6cbd0b51fc30db8df38c6d83f9c70
Mariana Moraes <mariana@siasp.com.br>
2023-09-16 11:14:34 -0300

    Estruturando e estilizando página de sucesso com dados dinâmicos do cliente

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -8,7 +8,7 @@ export default async function handler (req: NextApiRequest, res: NextApiResponse
     if (!priceId) return res.status(404).json({ error: 'Price not found'})
 
     const checkoutSession = await stripe.checkout.sessions.create({
-        success_url: `${process.env.APP_URL}/success`,
+        success_url: `${process.env.APP_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${process.env.APP_URL}/`,
         mode: 'payment',
         line_items: [
